465e46863785a7602bcda10e32856b0c9e83abdc
Amandeep <amandeep.bhamra@hotmail.com>
2013-02-14 16:13:07 +0530

    user model created with just username

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lighthouseapp::Application.config.secret_token = '64439982f1f8e8b3610e2f2b836cb1962f990845358c4c6706de6e9629aec4a08a674bad0b60dca454b6a1f185573da497312e1691ae8e5ca287ce47953aaba5'
