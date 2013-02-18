a5654f886e5dc97642d879a88f088512cb6a9163
Amandeep <amandeep.bhamra@hotmail.com>
2013-02-18 18:15:05 +0530

    Documents can now be attached and downloadable

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,8 @@
 class Ticket < ActiveRecord::Base
-  attr_accessible :description, :title, :project_id
+  attr_accessible :description, :title, :project_id, :document
 
   belongs_to :project
   belongs_to :user
 
+  has_attached_file :document, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
