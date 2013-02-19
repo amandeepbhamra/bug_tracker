349b72b1942b20cb07e9edffbc6a4e2175ead8b3
Amandeep <amandeep.bhamra@hotmail.com>
2013-02-19 14:30:46 +0530

    Minor changes

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,6 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :document, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+  Ticket_status = {1=> "New", 2 => "Open", 3 => "Hold", 4 => "Resolved", 5 => "Closed"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218120323) do
+ActiveRecord::Schema.define(:version => 20130219085946) do
 
   create_table "projects", :force => true do |t|
     t.string   "name",        :null => false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20130218120323) do
     t.string   "document_content_type"
     t.integer  "document_file_size"
     t.datetime "document_updated_at"
+    t.integer  "status"
   end
 
   create_table "users", :force => true do |t|
