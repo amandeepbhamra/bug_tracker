0455862effa979779a661083eb1fbda1c076dcdc
Amandeep <amandeep.bhamra@hotmail.com>
2013-02-19 15:29:18 +0530

    minor changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ActiveRecord::Base
   attr_accessible :description, :name
 
   has_many :tickets
+  has_and_belongs_to_many :members, :class_name => "User"
   belongs_to :user
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130219085946) do
+ActiveRecord::Schema.define(:version => 20130219095104) do
 
   create_table "projects", :force => true do |t|
     t.string   "name",        :null => false
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(:version => 20130219085946) do
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
   end
+
+  create_table "projects_users", :id => false, :force => true do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+  end
+
+  add_index "projects_users", ["project_id", "user_id"], :name => "index_projects_users_on_project_id_and_user_id"
+  add_index "projects_users", ["user_id", "project_id"], :name => "index_projects_users_on_user_id_and_project_id"
 
   create_table "tickets", :force => true do |t|
     t.string   "title"
