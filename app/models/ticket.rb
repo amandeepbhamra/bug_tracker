317913e72d8167905e6f0994b27001b7b62bbcd7
Amandeep <amandeep.bhamra@hotmail.com>
2013-02-19 14:52:53 +0530

    added status to tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,10 +1,18 @@
 class Ticket < ActiveRecord::Base
-  attr_accessible :description, :title, :project_id, :document
+  attr_accessible :description, :title, :project_id, :document, :status
 
   belongs_to :project
   belongs_to :user
 
   has_attached_file :document, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
-  Ticket_status = {1=> "New", 2 => "Open", 3 => "Hold", 4 => "Resolved", 5 => "Closed"}
+  TICKET_STATES = {1=> "New", 2 => "Open", 3 => "Hold", 4 => "Resolved", 5 => "Closed"}
+
+  def ticket_status
+  	TICKET_STATES[status]
+  end
+  
+  def self.ticket_status_array
+  	TICKET_STATES.to_a.sort
+  end
 end
