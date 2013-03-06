ca2530e4cf6c6a01a9a0437a9e3716e202744fe5
Amandeep <amandeep.bhamra@hotmail.com>
2013-03-06 12:08:04 +0530

    minor changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name, :user_ids
 
-  has_many :tickets
-  has_many :roles
+  has_many :tickets, :dependent => :destroy
+  has_many :roles, :dependent => :destroy
   
   has_and_belongs_to_many :members, :class_name => "User", :uniq => true, :join_table => "projects_users"
   
