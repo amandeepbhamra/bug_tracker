f7777b8db3105b9b63040963880ae765a33f12ab
Amandeep <amandeep.bhamra@hotmail.com>
2013-03-08 20:02:59 +0530

    attachment can be done in ticket but single

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,7 +1,7 @@
 module TicketsHelper
-	def add_attachment_link(name) 
-	    link_to_function(name) do |ticket| 
-	      	ticket.insert_html :bottom, :attachments, :partial => 'attachment', :object => Attachment.new 
-	    end 
-  	end
+	def add_attachment_link(name)
+  		link_to_function name do |ticket|
+    		ticket.insert_html :bottom, :attachments, :partial => 'attachment', :object => Attachment.new
+  		end
+	end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,14 @@
 class Comment < ActiveRecord::Base
 	attr_accessible :commentor, 
 					:content, 
-					:ticket_id
+					:ticket_id,
+					:attachments_attributes
 
   	has_many :attachments, :as => :attachable, :dependent => :destroy
 
   	belongs_to :user
+
+  	accepts_nested_attributes_for :attachments, :allow_destroy => true
 
 	validates :content, :commentor, :presence => true 
 end
