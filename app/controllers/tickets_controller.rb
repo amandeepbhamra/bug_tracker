5f42e77309dabf2af887b81cf7e406cae5258e32
Amandeep <amandeep.bhamra@hotmail.com>
2013-03-13 16:57:43 +0530

    conflicts resolved

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -38,8 +38,8 @@ class TicketsController < ApplicationController
     @ticket = @project.tickets.build(params[:ticket])
     respond_to do |format|
       if @ticket.save
-        Notify.ticket_creation_notification(@user, @project, @ticket).deliever
-        Notify.notify_to_whom_ticket_is_assigned(@user, @project, @ticket).deliever
+        Notify.ticket_creation_notification(@user, @project, @ticket).deliver
+        Notify.notify_to_whom_ticket_is_assigned(@user, @project, @ticket).deliver
         format.html { redirect_to project_ticket_path(@project,@ticket), notice: 'Ticket was successfully created.' }
       else
         format.html { render action: "new" }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        Notify.user_profile_update_notification(@user).deliever
+        Notify.user_profile_update_notification(@user).deliver
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
